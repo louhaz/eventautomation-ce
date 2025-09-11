2674cb810be615369d837625f2eaf1b4e112177f
louise-hazell <louise.hazell@ibm.com>
2025-09-11 10:52:08 +0200

    create eta table

--- components/postgres/base/tables.sql
+++ components/postgres/base/tables.sql
@@ -1,4 +1,7 @@
-CREATE TABLE eta (
+CREATE SCHEMA IF NOT EXISTS eta_schema;
+
+
+CREATE TABLE IF NOT EXISTS eta_schema.eta (
     route_id TEXT NOT NULL,
     sequence_number TEXT NOT NULL,
     vehicle_id TEXT NOT NULL,
@@ -6,3 +9,7 @@ CREATE TABLE eta (
     delivered TIMESTAMP WITHOUT TIME ZONE,
     PRIMARY KEY (route_id, sequence_number)
 );
+
+GRANT ALL PRIVILEGES ON SCHEMA eta_schema TO "postgres-db";
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA eta_schema TO "postgres-db";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres-db" IN SCHEMA eta_schema GRANT ALL ON TABLES TO "postgres-db";
